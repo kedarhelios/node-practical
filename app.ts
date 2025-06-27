04b9ea1c38c2b08f23d6c570572a65e83ae6a3c7
Kedar Vyas <kedar.vyas@heliossolutions.co>
2025-06-27 17:07:25 +0530

    implemented login page with ejs

--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import routes from "./routes";
 import ApiError from "./utils/ApiError";
 import { errorConverter, errorHandler } from "./utils/errorHandlers";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/login", (req: Request, res: Response, next: NextFunction) => {
+    res.render("login");
+});
 
 app.use(bodyParser.json());
 
